1f65802090a54e2456a0c06357b33e0a7aa7bcc0
hebron <hebron@tallykhata.com>
2023-01-22 11:47:01 +0600

    [update] added assertion with bangla text for all the tests in TallyTestRunner

--- src/test/java/testRunners/TallyTestRunner.java
+++ src/test/java/testRunners/TallyTestRunner.java
@@ -1,8 +1,7 @@
 package testRunners;
 
-import jdk.jshell.execution.Util;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.json.simple.parser.ParseException;
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -14,6 +13,7 @@ import utils.Utils;
 import java.io.IOException;
 
 public class TallyTestRunner extends Setup {
+    Dotenv dotenv = Dotenv.load();
     @BeforeTest
     public void register() {
         RegistrationScreen rScreen = new RegistrationScreen(driver);
@@ -28,25 +28,14 @@ public class TallyTestRunner extends Setup {
 //        String customerPhoneNumber = Utils.randomPhoneNumber();
         String customerPhoneNumber = "01517169846";
         String confirmationMessage = tScreen.addCustomerWithNameAndPhone(customerName, customerPhoneNumber);
-        String[] arrOfStr = confirmationMessage.split("-");
-        String[] bangla = new String[]{"কে টালিতে যোগ করা হয়েছে।"};
-//        String bangla1 = "কে টালিতে যোগ করা হয়েছে।";
-//        String rawString = "Entwickeln Sie mit Vergnügen";
-//        ByteBuffer buffer = StandardCharsets.UTF_8.encode(rawString);
-
-//        String utf8EncodedString = StandardCharsets.UTF_8.decode(buffer).toString();
-//        System.out.println("actual "+confirmationMessage);
-//        String expected = "\"" + customerName + "\"- " + " কে টালিতে যোগ করা হয়েছে।";
-//        System.out.println("after splitting "+ arrOfStr[1]);
-//        System.out.println("bangla "+ bangla[0].getClass().getName());
         Utils.saveJsonList(customerPhoneNumber, customerName,"customers");
-//        Assert.assertTrue(confirmationMessage.contains(arrOfStr[1]));
+        Assert.assertTrue(confirmationMessage.contains(dotenv.get("PARTIAL_TEXT_AFTER_CREATING_CUSTOMER_OR_SUPPLIER")));
     }
     @Test(priority = 2, description = "Create customer with all the input along with date and image")
     public void addCustomerFromContactBookWithAllInput() {
         TallyScreen tScreen = new TallyScreen(driver);
         String confirmationMessage = tScreen.addCustomerFromContactBookWithAllInput("12");
-        System.out.println(confirmationMessage);
+        Assert.assertTrue(confirmationMessage.contains(dotenv.get("PARTIAL_TEXT_AFTER_CREATING_CUSTOMER_OR_SUPPLIER")));
     }
     @Test(priority = 3, description = "Create supplier with only name and phone")
     public void addSupplierWithNameAndPhone() throws Exception {
@@ -55,64 +44,61 @@ public class TallyTestRunner extends Setup {
         String supplierPhoneNumber = Utils.randomPhoneNumber();
         String confirmationMessage = tScreen.addSupplierWithNameAndPhone(supplierName, supplierPhoneNumber);
         Utils.saveJsonList(supplierPhoneNumber, supplierName,"customers");
-        System.out.println(confirmationMessage);
+        Assert.assertTrue(confirmationMessage.contains(dotenv.get("PARTIAL_TEXT_AFTER_CREATING_CUSTOMER_OR_SUPPLIER")));
     }
     @Test(priority = 4, description = "Create supplier with all the input along with date and image")
     public void addSupplierFromContactBookWithAllInput() {
         TallyScreen tScreen = new TallyScreen(driver);
         String confirmationMessage = tScreen.addSupplierFromContactBookWithAllInput("11");
-        System.out.println(confirmationMessage);
+        Assert.assertTrue(confirmationMessage.contains(dotenv.get("PARTIAL_TEXT_AFTER_CREATING_CUSTOMER_OR_SUPPLIER")));
     }
     @Test(priority = 5, description = "Loop through customer list and print out length of data")
     public void loopThroughCustomerData() {
         TallyScreen tScreen = new TallyScreen(driver);
         int actualTotalCustomerLength = tScreen.objectCustomerDetailsContainer.size();
         System.out.println("Customer length " + actualTotalCustomerLength);
-//        Assert.assertEquals(actualTotalCustomerLength, 2);
+        Assert.assertEquals(actualTotalCustomerLength, 4);
     }
-    @Test(priority = 6, description = "Edit first customer from  the customer list")
-    public void editFirstCustomerPelam() throws IOException, ParseException, InterruptedException {
+    @Test(priority = 6, description = "Edit PELAM of first customer from  the customer list")
+    public void editFirstCustomerPelam() {
         TallyScreen tScreen = new TallyScreen(driver);
         String confirmationMessage = tScreen.editFirstCustomerPelam("12");
-        System.out.println("confirmation Message after edit " + confirmationMessage);
-//        Assert.assertEquals(customerFound, 1);
+        Assert.assertTrue(confirmationMessage.contains(dotenv.get("TEXT_AFTER_UPDATING_PELAM_DILAM")));
     }
     @Test(priority = 7, description = "Update first customer name from  the customer list")
-    public void editFirstCustomerName() throws IOException, ParseException, InterruptedException {
+    public void editFirstCustomerName() throws InterruptedException {
         TallyScreen tScreen = new TallyScreen(driver);
         String confirmationMessage = tScreen.editFirstCustomerName(Utils.randomCustomerName());
-        System.out.println("confirmation Message after name update " + confirmationMessage);
-//        Assert.assertEquals(customerFound, 1);
+        Assert.assertTrue(confirmationMessage.contains(dotenv.get("TEXT_AFTER_UPDATING_NAME_OR_MOBILE_OF_CUSTOMER_OR_SUPPLIER")));
     }
     @Test(priority = 8, description = "Search for the last customer added in the customer list")
     public void searchForLastAddedCustomer() throws IOException, ParseException, InterruptedException {
         TallyScreen tScreen = new TallyScreen(driver);
         int customerFound = tScreen.searchLastAddedCustomer(Utils.getLastAddedCustomerName("customers"));
-        System.out.println("Customer found " + customerFound);
-//        Assert.assertEquals(customerFound, 1);
+        Assert.assertEquals(customerFound, 1);
     }
     @Test(priority = 9, description = "Close eye Button and check that all numbers ar hidden")
-    public void closeEyeButton() throws IOException, ParseException, InterruptedException {
+    public void closeEyeButton() throws InterruptedException {
         TallyScreen tScreen = new TallyScreen(driver);
         String totalPabo = tScreen.closeEyeButton();
-        Assert.assertEquals(totalPabo, "- - -");
+        Assert.assertEquals(totalPabo, dotenv.get("TEXT_AFTER_CLOSING_EYE_BUTTON"));
     }
     @Test(priority = 10, description = "Add filter for Showing only customer")
-    public void addFilterOnlyCustomer() throws IOException, ParseException, InterruptedException {
+    public void addFilterOnlyCustomer() throws InterruptedException {
         TallyScreen tScreen = new TallyScreen(driver);
         int customerFound = tScreen.addFilter();
-        System.out.println("Customer found " + customerFound);
+        Assert.assertEquals(customerFound, 2);
     }
     @Test(priority = 11, description = "Tagada pathai to customers")
-    public void tagadaPathai() throws IOException, ParseException, InterruptedException {
+    public void tagadaPathai() {
         TallyScreen tScreen = new TallyScreen(driver);
-        String tagadaSentConfirmationMessage = tScreen.tagadaPathai();
-        System.out.println("tagada Sent Confirmation Message " + tagadaSentConfirmationMessage);
+        String confirmationMessage = tScreen.tagadaPathai();
+        Assert.assertTrue(confirmationMessage.contains(dotenv.get("TEXT_AFTER_SENDING_TAGADA_MESSAGE")));
     }
     @Test(priority = 12, description = "Delete Last customer from the list")
-    public void deleteLastCustomer() throws IOException, ParseException, InterruptedException {
+    public void deleteLastCustomer() throws InterruptedException {
         TallyScreen tScreen = new TallyScreen(driver);
-        String deleteCustomerConfirmationMessage = tScreen.deleteLastCustomer();
-        System.out.println("delete Confirmation Message " + deleteCustomerConfirmationMessage);
+        String confirmationMessage = tScreen.deleteLastCustomer();
+        Assert.assertTrue(confirmationMessage.contains(dotenv.get("PARTIAL_TEXT_AFTER_DELETING_CUSTOMER_OR_SUPPLIER")));
     }
 }
