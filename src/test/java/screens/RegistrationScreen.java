f2dabdcaa3c0672ca2434e8194dae600eb254721
hebron <hebron@tallykhata.com>
2023-02-07 21:59:34 +0600

    ✅:Add test cases for unverified account where an unverified account is created, and then it is verified

--- src/test/java/screens/RegistrationScreen.java
+++ src/test/java/screens/RegistrationScreen.java
@@ -3,10 +3,16 @@ package screens;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 
 public class RegistrationScreen {
     public RegistrationScreen(AndroidDriver driver) {
@@ -27,6 +33,21 @@ public class RegistrationScreen {
     WebElement inputShopName;
     @FindBy(id = "com.progoti.tallykhata:id/toolbar_title")
     public WebElement textShopTitle;
+    @FindBy(id = "com.progoti.tallykhata:id/iv_alert")
+    WebElement iconAlert;
+    @FindBy(id = "com.progoti.tallykhata:id/btn_verify_number")
+    WebElement btnVerifyKori;
+    @FindBy(id = "com.progoti.tallykhata:id/item_menu")
+    WebElement tabMenu;
+    @FindBy(id = "com.progoti.tallykhata:id/tv_title")
+    List<WebElement> tabMenuOptions;
+    @FindBy(id = "com.progoti.tallykhata:id/back_button")
+    WebElement btnBack;
+    @FindBy(id = "com.progoti.tallykhata:id/ivDataBackup")
+    WebElement btnDataBackup;
+    @FindBy(id = "com.progoti.tallykhata:id/tvDataBackupMessage")
+    WebElement textDataBackupMessage;
+
 
     public boolean insertPhoneAndClickButton(String mobileNumberToInsert) {
         inputMobileNumber.sendKeys(mobileNumberToInsert);
@@ -36,6 +57,7 @@ public class RegistrationScreen {
         }
         return isButtonEnabled;
     }
+
     public boolean insertOtpAndClickButton(AndroidDriver driver) {
         inputOtpCode1.click();
         Actions action = new Actions(driver);
@@ -46,6 +68,7 @@ public class RegistrationScreen {
         }
         return isButtonEnabled;
     }
+
     public boolean insertShopNameAndClickButton(String shop_name) {
         inputShopName.sendKeys(shop_name);
         boolean isButtonEnabled = btnNext.isEnabled();
@@ -53,5 +76,32 @@ public class RegistrationScreen {
             btnNext.click();
         }
         return isButtonEnabled;
+    }
+
+    public String createUnverifiedAccount(String phone, String name, AndroidDriver driver) {
+        insertPhoneAndClickButton(phone);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(90));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.progoti.tallykhata:id/et_shop_name")));
+        insertShopNameAndClickButton(name);
+        return textShopTitle.getText();
+    }
+
+    public boolean verifyMobile(AndroidDriver driver) {
+        iconAlert.click();
+        btnVerifyKori.click();
+        btnNext.click();
+        insertOtpAndClickButton(driver);
+        return driver.findElements(By.id("com.progoti.tallykhata:id/iv_alert")).isEmpty();
+    }
+
+    public String verifyDataBackup() throws InterruptedException {
+        tabMenu.click();
+        Thread.sleep(2000);
+        tabMenuOptions.get(7).click();
+        btnDataBackup.click();
+        Thread.sleep(5000);
+        String dataBackupMessage = textDataBackupMessage.getText();
+        btnBack.click();
+        return dataBackupMessage;
     }
 }
---
+++ src/test/java/testRunners/UnverifiedRegistrationTestRunner.java
@@ -0,0 +1,35 @@
+package testRunners;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import screens.RegistrationScreen;
+import setup.Setup;
+import utils.Utils;
+
+public class UnverifiedRegistrationTestRunner extends Setup {
+    Dotenv dotenv = Dotenv.load();
+
+    @Test(priority = 1)
+    public void createUnverifiedAccount() throws InterruptedException {
+        RegistrationScreen rScreen = new RegistrationScreen(driver);
+        String shopName = Utils.randomShopName();
+        String actualShopName = rScreen.createUnverifiedAccount(Utils.randomPhoneNumber(), shopName, driver);
+        Assert.assertEquals(actualShopName, shopName);
+    }
+
+    @Test(priority = 2)
+    public void verifyMobile() {
+        RegistrationScreen rScreen = new RegistrationScreen(driver);
+        boolean isUnavailable = rScreen.verifyMobile(driver);
+        System.out.println("verifyMobile " + isUnavailable);
+        Assert.assertTrue(isUnavailable);
+    }
+
+    @Test(priority = 3)
+    public void verifyDataBackup() throws InterruptedException {
+        RegistrationScreen rScreen = new RegistrationScreen(driver);
+        String confirmationMessage = rScreen.verifyDataBackup();
+        Assert.assertTrue(confirmationMessage.contains(dotenv.get("TEXT_DATA_BACKUP_NEWA_ACHE")));
+    }
+}
